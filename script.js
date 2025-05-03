07c8a9f22ea9c24bd6ce4c607d93bec27914d171
ddorangie <nuvelax02232@gmail.com>
2025-05-03 12:21:50 +0300

    correct js

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 const buttons = document.querySelectorAll('.icon-button');
+const questions = document.querySelectorAll('.question')
+
+function showAnswer (element) {
+    const block = element.closest('.block-question');
+    const answer = block.querySelector('.answer');
+    const icon = block.querySelector('.icon-button');
+
+    answer.classList.toggle('show');
+    icon.classList.toggle('opened');
+}
 
 buttons.forEach(btn => {
-    btn.addEventListener('click', () => {      // событие по клику
-        btn.classList.toggle('opened');        // меняем кнопку с плюса на минус
+    btn.addEventListener('click', () => showAnswer(btn));
+    
+});
 
-        const block = btn.closest('.block-question');
-        const answer = block.querySelector('.answer');
-
-        answer.classList.toggle('show');       // по клику показываем ответ
-    });
-})
-   
-
+questions.forEach(ques => {
+    ques.addEventListener('click', () => showAnswer(ques));
+    
+});
